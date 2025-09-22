5dfb2e3ab6ede294cba886da273ae6d534c3b485
fazara06 <faza.rs2001@gmail.com>
2025-09-22 20:56:28 +0700

    Add files via upload

---
+++ resources/views/partials/home.blade.php
@@ -0,0 +1,5 @@
+<svg {{ $attributes->merge(['class' => 'w-5 h-5']) }} xmlns="http://www.w3.org/2000/svg" 
+     fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M13 5v6h6"/>
+</svg>
